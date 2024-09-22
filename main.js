6b5dce414d7e0cf95ae6a3ccd83fbd869ce94979
JoacoP <joaquiinparellada@gmail.com>
2024-09-22 18:56:53 -0300

    proyecto integrador

---
+++ main.js
@@ -0,0 +1,36 @@
+import { renderCategorias } from "./src/services/categories.js";
+import { setInLocalstorage } from "./src/persistences/localStorage.js";
+import { handleSearchProductByName } from "./src/services/searchBar.js";
+import { openModal } from "./src/views/modal.js";
+import { handleGetProductsToStore } from "./src/views/store.js";
+import "./style.css";
+
+//==== APLICACOIN ====//
+
+export let categoriaActiva = null;
+
+export const setCategoriaActiva = (categoriaIn) => {
+  categoriaActiva = categoriaIn;
+};
+
+export let productoActivo = null;
+
+export const setProductoActivo = (productoIn) => {
+  productoActivo = productoIn;
+};
+
+handleGetProductsToStore();
+
+renderCategorias();
+
+const buttonAdd = document.getElementById("buttonAddElement");
+buttonAdd.addEventListener("click", () => {
+  openModal();
+});
+
+//buttonSearch
+
+const buttonSearch = document.getElementById("buttonSearch");
+buttonSearch.addEventListener("click", () => {
+  handleSearchProductByName();
+});
---
+++ src/services/searchBar.js
@@ -0,0 +1,14 @@
+import { handleGetProductLocalStorage } from "../persistences/localStorage";
+import { handleRenderList } from "../views/store";
+
+export const handleSearchProductByName = () => {
+  const inputHeader = document.getElementById("inputHeader");
+
+  const products = handleGetProductLocalStorage();
+
+  const result = products.filter((el) =>
+    el.nombre.toLowerCase().includes(inputHeader.value)
+  );
+
+  handleRenderList(result);
+};
